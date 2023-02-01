0c72812b9ded41a77071309675cecfdf6a9526cd
MD. ABDUL HAMIM <hamim15-5264@diu.edu.bd>
2023-02-01 22:02:57 +0600

    Create Teacher.java

---
+++ OOP/src/oop/Teacher.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package oop;
+
+/**
+ *
+ * @author Win-10
+ */
+public class Teacher {
+    String name, gender;
+    int phone ;
+    
+}
