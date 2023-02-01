7ec883bbc5713aa6e675045974c88f6fcb29e429
MD. ABDUL HAMIM <hamim15-5264@diu.edu.bd>
2023-02-01 22:02:53 +0600

    Create Teacher2.java

---
+++ OOP/src/oop/Teacher2.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package oop;
+
+/**
+ *
+ * @author Win-10
+ */
+public class Teacher2 {
+    String name, gender;
+    int phone ;
+    
+    void printinfo(){
+    System.out.println("Name: "+name);
+    System.out.println("Gender: "+gender);
+    System.out.println("Phone: "+phone);
+}
+    
+}
