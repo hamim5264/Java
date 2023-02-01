02c5968394faa1ed43fed6d50f04c45c0a180da6
MD. ABDUL HAMIM <hamim15-5264@diu.edu.bd>
2023-02-01 20:50:27 +0600

    Create Basic1.java

---
+++ BasicJava/src/basicjava/Basic1.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package basicjava;
+
+/**
+ *
+ * @author Win-10
+ */
+public class Basic1 {
+    public static void main (String[] args){
+        
+        System.out.println("Hamim");
+        System.out.println("01724879284");
+    }
+    
+}
