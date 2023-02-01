aa3f63620125a9087c6a93d15495061ede3ad8e5
MD. ABDUL HAMIM <hamim15-5264@diu.edu.bd>
2023-02-01 22:02:50 +0600

    Create TestCons.java

---
+++ OOP/src/oop/TestCons.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package oop;
+
+/**
+ *
+ * @author Win-10
+ */
+public class TestCons {
+    public static void main(String[] args) {
+        Constructor info = new Constructor("Hamim","Male",1724879284);
+        info.printinfo();
+        Constructor info2 = new Constructor("Shahina","Female", 1719865871);
+        info2.printinfo();
+    }
+    
+}
