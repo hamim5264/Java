91dc6aab8e36829ec2593e0517a96d0705fb9559
MD. ABDUL HAMIM <hamim15-5264@diu.edu.bd>
2023-02-01 22:15:39 +0600

    Create Randomdemo.java

---
+++ JavaFile1/src/string/Randomdemo.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package string;
+
+import java.util.Random;
+
+/**
+ *
+ * @author Win-10
+ */
+public class Randomdemo {
+    Random rand = new Random ();
+    int randomnumber = rand.nextInt(10)+1;
+    System.out.println(randomnumber);
+    
+}
