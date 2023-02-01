9ccf4af1c542f733aea2324766692e0f9d70b58a
MD. ABDUL HAMIM <hamim15-5264@diu.edu.bd>
2023-02-01 22:15:51 +0600

    Create String4.java

---
+++ JavaFile1/src/string/String4.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package string;
+
+/**
+ *
+ * @author Win-10
+ */
+public class String4 {
+    public static void main(String[] args) {
+        String s1 = "This is my country";
+        System.out.println(s1);
+        String s2 = s1.replace("i", "j");
+        System.out.println(s2);
+        
+        String [] s3 = s1.split(" ");
+        for(String X: s3){
+        System.out.println(s3);
+        }
+    }
+    
+}
