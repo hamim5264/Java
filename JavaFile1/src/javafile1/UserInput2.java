acbe75d470d715b5f24a0ac49ce8592b9b6cd5d7
MD. ABDUL HAMIM <hamim15-5264@diu.edu.bd>
2023-02-01 22:15:20 +0600

    Create UserInput2.java

---
+++ JavaFile1/src/javafile1/UserInput2.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javafile1;
+
+/**
+ *
+ * @author Win-10
+ */
+import java.util.Scanner;
+
+public class UserInput2 {
+    public static void main (String[] args){
+        Scanner input = new Scanner(System.in);
+        int num;
+        System.out.print("Enter Any Number: ");
+        num = input.nextInt();
+        System.out.print("Number= "+num);
+        
+    }
+    
+}
