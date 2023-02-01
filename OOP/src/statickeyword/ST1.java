3f1ff929722ce5c25dd080f994c9308f8f43e534
MD. ABDUL HAMIM <hamim15-5264@diu.edu.bd>
2023-02-01 22:02:32 +0600

    Create ST1.java

---
+++ OOP/src/statickeyword/ST1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package statickeyword;
+
+/**
+ *
+ * @author Win-10
+ */
+public class ST1 {
+    public static void main(String[] args) {
+        Student s1 = new Student ("Hamim", 221);
+        s1.printinfo();
+        Student s2 = new Student ("Rafi", 222);
+        s2.printinfo();
+    }
+    
+}
