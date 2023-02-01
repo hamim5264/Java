8ce9148bcec6757287935146d55ed85ef3b6486c
MD. ABDUL HAMIM <hamim15-5264@diu.edu.bd>
2023-02-01 21:45:28 +0600

    Create DataType.java

---
+++ DataType/src/datatype/DataType.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package datatype;
+
+/**
+ *
+ * @author Win-10
+ */
+public class DataType {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        boolean b = true;
+        char c = 'a';
+        short s = 4325;
+        int i = 45;
+        float f = 4.5f;
+        double d = 54.65;
+        
+        System.out.println("b="+b);
+        System.out.println("c="+c);
+        System.out.println("i="+i);
+        System.out.println("f="+f);
+        System.out.println("d="+d);
+    }
+    
+}
