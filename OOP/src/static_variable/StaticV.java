3635ff9491fca6827f1a2e92f1613ff6f37b2b59
MD. ABDUL HAMIM <hamim15-5264@diu.edu.bd>
2023-02-01 22:02:43 +0600

    Create StaticV.java

---
+++ OOP/src/static_variable/StaticV.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package static_variable;
+
+/**
+ *
+ * @author Win-10
+ */
+public class StaticV {
+    static String varsity = "Daffodil International University";
+    
+}
