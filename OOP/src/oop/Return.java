2d8a5f49e007cbbd0c805017c37fb1ea9ee406f0
MD. ABDUL HAMIM <hamim15-5264@diu.edu.bd>
2023-02-01 22:03:08 +0600

    Create Return.java

---
+++ OOP/src/oop/Return.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package oop;
+
+/**
+ *
+ * @author Win-10
+ */
+public class Return {
+    public static void main(String[] args) {
+        ReturnValue r1 = new ReturnValue ();
+            System.out.println("value: "+r1.square(5));
+        }
+    }
+    
+
