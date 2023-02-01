d2a5352d31272a27726057c50ce2baf2a4e610e9
MD. ABDUL HAMIM <hamim15-5264@diu.edu.bd>
2023-02-01 22:12:58 +0600

    Create Assignment10.java

---
+++ JavaFile1/src/javafile1/Assignment10.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javafile1;
+
+/**
+ *
+ * @author Win-10
+ */
+public class Assignment10 {
+    public static void main(String[] args) {
+        int i, sum=0;
+        for (i=1; i<=10;i++){
+            sum = sum+i; 
+        }
+        System.out.println("Sum="+sum);
+        
+    }
+    
+}
