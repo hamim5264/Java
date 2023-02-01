63ef074bcdd5e02e637f30a78b3f011d1192fbdc
MD. ABDUL HAMIM <hamim15-5264@diu.edu.bd>
2023-02-01 22:03:30 +0600

    Create DF.java

---
+++ OOP/src/oop/DF.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package oop;
+
+/**
+ *
+ * @author Win-10
+ */
+public class DF {
+    String name, gender;
+    int phone;
+    DF (){
+        System.out.println("No value");
+    }
+    DF(String st, String gd, int ph){
+        name = st;
+        gender = gd;
+        phone = ph;
+    }
+    void printinfo(){
+        System.out.println("Name: "+name);
+        System.out.println("Gender: "+gender);
+        System.out.println("Phone: "+phone);
+        System.out.println("\n");
+    }
+    
+}
