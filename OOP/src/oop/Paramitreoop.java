a06c02277d1a56dfe6cc26dc5f0064d14e607a14
MD. ABDUL HAMIM <hamim15-5264@diu.edu.bd>
2023-02-01 22:03:11 +0600

    Create Paramitreoop.java

---
+++ OOP/src/oop/Paramitreoop.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package oop;
+
+/**
+ *
+ * @author Win-10
+ */
+public class Paramitreoop {
+    public static void main(String[] args) {
+        Paramitre info = new Paramitre ();
+        info.mainifo("Hamim", "Male", 1724879284);
+        info.printinfo();
+        
+        Paramitre info2 = new Paramitre ();
+        info2.mainifo("Shahina", "Female", 1719865871);
+        info2.printinfo();
+    }
+    
+}
