b0338a795eeaf9f680c278a70bc1e3393ee2c560
MD. ABDUL HAMIM <hamim15-5264@diu.edu.bd>
2023-02-01 22:13:24 +0600

    Create AssignmentOp.java

---
+++ JavaFile1/src/javafile1/AssignmentOp.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javafile1;
+
+/**
+ *
+ * @author Win-10
+ */
+public class AssignmentOp {
+    public static void main (String[] args){
+        int x = 3;
+        int y = 4;
+        x+=y;
+        System.out.println("X="+x);
+        x-=y;
+        System.out.println("X="+x);
+        x*=y;
+        System.out.println("X="+x);
+        x/=y;
+        System.out.println("X="+x);
+        x%=y;
+        System.out.println("X="+x);
+    }
+    
+}
