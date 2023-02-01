688f143b96c10d9e59b9cd2dde2952707fae6e29
MD. ABDUL HAMIM <hamim15-5264@diu.edu.bd>
2023-02-01 22:02:47 +0600

    Create TestDf.java

---
+++ OOP/src/oop/TestDf.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package oop;
+
+/**
+ *
+ * @author Win-10
+ */
+public class TestDf {
+    public static void main(String[] args) {
+        DF info1 = new DF ("Hamim", "Male", 1724879284);
+        info1.printinfo();
+        DF info2 = new DF ();
+        info2.printinfo();
+    }
+}
