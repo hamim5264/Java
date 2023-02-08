32f42d7d6104738b58618edbf974435f6114d0af
MD. ABDUL HAMIM <hamim15-5264@diu.edu.bd>
2023-02-08 22:56:29 +0600

    Create Test2.java

---
+++ OOP/src/interfase/Test2.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package interfase;
+
+/**
+ *
+ * @author Win-10
+ */
+public class Test2 {
+    public static void main(String[] args) {
+        C ob1 = new C ();
+        ob1.play();
+    }
+   
+}
