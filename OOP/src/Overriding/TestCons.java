3d8c23b85ee32a64d9d690b35e08557e293d92e9
MD. ABDUL HAMIM <hamim15-5264@diu.edu.bd>
2023-02-08 22:55:16 +0600

    Create TestCons.java

---
+++ OOP/src/Overriding/TestCons.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Overriding;
+
+/**
+ *
+ * @author Win-10
+ */
+public class TestCons {
+    public static void main(String[] args) {
+        Supercons2 s1 = new Supercons2();
+      
+    }
+    
+}
