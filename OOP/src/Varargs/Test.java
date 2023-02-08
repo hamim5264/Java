06d29434fd53632e431c06392e3df6f32ceb9f17
MD. ABDUL HAMIM <hamim15-5264@diu.edu.bd>
2023-02-08 22:53:40 +0600

    Create Test.java

---
+++ OOP/src/Varargs/Test.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Varargs;
+
+/**
+ *
+ * @author Win-10
+ */
+public class Test {
+    public static void main(String[] args) {
+        Adddemo o = new Adddemo ();
+            o.add(10,20,30);
+            o.add(10,20,30,40);
+            o.add(10,20,30,40,50);
+        
+    }
+    
+}
