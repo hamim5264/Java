2c2ef5823042ccf82b037d0d46ac76764650bdc8
MD. ABDUL HAMIM <hamim15-5264@diu.edu.bd>
2023-02-08 22:56:49 +0600

    Create Test2.java

---
+++ OOP/src/inheritance/Test2.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package inheritance;
+
+/**
+ *
+ * @author Win-10
+ */
+public class Test2 {
+    public static void main(String[] args) {
+        Teacher2 t1 = new Teacher2 ();
+        t1.setName("Hamim");
+        t1.setAge(21);
+        t1.setQuali("B.Sc in CSE");
+        System.out.println(t1.getName());
+        System.out.println(t1.getAge());
+        System.out.println(t1.getQuali());
+        
+    }
+    
+}
