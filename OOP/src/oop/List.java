dc8ddeb9d4e11e7994610ee8714a9142bacafee0
MD. ABDUL HAMIM <hamim15-5264@diu.edu.bd>
2023-02-08 22:56:20 +0600

    Create List.java

---
+++ OOP/src/oop/List.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package oop;
+
+/**
+ *
+ * @author Win-10
+ */
+public class List {
+    String name, classname;
+    int id;
+    List (int id, String name, String classname ){
+        this.id = id;
+        this.name = name;
+        this.classname = classname;
+    }
+    
+}
