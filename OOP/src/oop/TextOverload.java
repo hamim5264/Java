bddedd7105602b6de76f4101118b59f57ce7ab02
MD. ABDUL HAMIM <hamim15-5264@diu.edu.bd>
2023-02-08 22:56:00 +0600

    Create TextOverload.java

---
+++ OOP/src/oop/TextOverload.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package oop;
+
+/**
+ *
+ * @author Win-10
+ */
+public class TextOverload {
+    public static void main(String[] args) {
+        Overload o1 = new Overload ();
+        o1.add(10, 20);
+        o1.add(10.5, 10.2);
+        o1.add(10, 20, 30);
+        o1.add();
+    }
+    
+}
