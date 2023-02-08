6a26f78ed7f1ee972b268eb2063ac81370a4094c
MD. ABDUL HAMIM <hamim15-5264@diu.edu.bd>
2023-02-08 22:55:41 +0600

    Create Supercons.java

---
+++ OOP/src/Overriding/Supercons.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Overriding;
+
+/**
+ *
+ * @author Win-10
+ */
+public class Supercons {
+    Supercons(){
+        System.out.println("This is A");
+    }
+    
+}
