60b6569f1d309666ed06925613e37ba4aa8c1ba8
MD. ABDUL HAMIM <hamim15-5264@diu.edu.bd>
2023-02-08 22:55:43 +0600

    Create Person.java

---
+++ OOP/src/Overriding/Person.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Overriding;
+
+/**
+ *
+ * @author Win-10
+ */
+public class Person {
+    String name;
+    int age;
+    void printinfo (){
+        System.out.println("Name: "+name);
+        System.out.println("Age: "+age);
+    }
+    
+}
