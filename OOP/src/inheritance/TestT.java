9577db970c9893c707cc640eeff458f4264e92dd
MD. ABDUL HAMIM <hamim15-5264@diu.edu.bd>
2023-02-08 22:56:46 +0600

    Create TestT.java

---
+++ OOP/src/inheritance/TestT.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package inheritance;
+
+/**
+ *
+ * @author Win-10
+ */
+public class TestT {
+    public static void main(String[] args) {
+        Teacher t1 = new Teacher ();
+        t1.name = "Hamim";
+        t1.age = 21;
+        t1.quali = "B.Sc in CSE";
+        t1.printinfo2();
+    }
+    
+}
