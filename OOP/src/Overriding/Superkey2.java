84ba5ae81253a3f1225bde7fb2f5f410235d6119
MD. ABDUL HAMIM <hamim15-5264@diu.edu.bd>
2023-02-08 22:55:34 +0600

    Create Superkey2.java

---
+++ OOP/src/Overriding/Superkey2.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Overriding;
+
+/**
+ *
+ * @author Win-10
+ */
+public class Superkey2 extends Superkey {
+    @Override
+    void print (){
+        super.print();
+      System.out.println("This is B");
+    }
+    
+}
