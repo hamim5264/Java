0a95627abc8013008853afa3dc574925403233e9
MD. ABDUL HAMIM <hamim15-5264@diu.edu.bd>
2023-02-08 22:55:46 +0600

    Create Keytext.java

---
+++ OOP/src/Overriding/Keytext.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Overriding;
+
+/**
+ *
+ * @author Win-10
+ */
+public class Keytext {
+    public static void main(String[] args) {
+        Thiskey t1 = new Thiskey ("hamim", 21);
+        t1.print();
+        Thiskey t2 = new Thiskey ("leon", 25, "black");
+        t2.print();
+    }
+    
+}
