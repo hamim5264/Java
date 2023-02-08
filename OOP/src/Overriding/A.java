3fa98d23fa331afa4775d1e3af42329b90825543
MD. ABDUL HAMIM <hamim15-5264@diu.edu.bd>
2023-02-08 22:55:55 +0600

    Create A.java

---
+++ OOP/src/Overriding/A.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Overriding;
+
+/**
+ *
+ * @author Win-10
+ */
+public class A {
+    int x = 10;
+    
+}
