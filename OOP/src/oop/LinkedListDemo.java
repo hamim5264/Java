d7fc520b2a6361321bdf91779734dd140486d99f
MD. ABDUL HAMIM <hamim15-5264@diu.edu.bd>
2023-02-08 22:56:23 +0600

    Create LinkedListDemo.java

---
+++ OOP/src/oop/LinkedListDemo.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package oop;
+
+import java.util.LinkedList;
+
+/**
+ *
+ * @author Win-10
+ */
+public class LinkedListDemo {
+    public static void main (String[] args){
+        LinkedList<String> country = new LinkedList();
+        country.add("Bangladesh");
+        country.add("India");
+        country.add("Pakistan");
+        System.out.println(country);
+    }
+    
+}
