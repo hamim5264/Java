03e379c78116c2333e9d16cbb9111da5366e41e2
MD. ABDUL HAMIM <hamim15-5264@diu.edu.bd>
2023-02-08 22:56:44 +0600

    Create A.java

---
+++ OOP/src/interfase/A.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package interfase;
+
+/**
+ *
+ * @author Win-10
+ */
+public interface A {
+    void play ();
+    
+}
