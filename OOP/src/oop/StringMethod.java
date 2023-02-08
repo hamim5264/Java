b4c5226fe7126150578c08dbb2bc2759872dbda6
MD. ABDUL HAMIM <hamim15-5264@diu.edu.bd>
2023-02-08 22:56:09 +0600

    Create StringMethod.java

---
+++ OOP/src/oop/StringMethod.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package oop;
+
+/**
+ *
+ * @author Win-10
+ */
+public class StringMethod {
+    public static void main(String[] args) {
+        Tostring t = new Tostring ("hamim", 21);
+          System.out.println(t);  
+        
+    }
+    
+}
