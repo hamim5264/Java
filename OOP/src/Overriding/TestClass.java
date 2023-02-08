f73c717fb002234f35c84ed6a3c7a0d5b11cce58
MD. ABDUL HAMIM <hamim15-5264@diu.edu.bd>
2023-02-08 22:55:30 +0600

    Create TestClass.java

---
+++ OOP/src/Overriding/TestClass.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Overriding;
+
+/**
+ *
+ * @author Win-10
+ */
+public class TestClass {
+    public static void main(String[] args) {
+        Car c1 = new Car ("white", 350,5);
+        c1.attribute();
+    }
+}
