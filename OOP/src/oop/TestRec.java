da0c56885865d4f1d7b4132de72cfcfc2aebc380
MD. ABDUL HAMIM <hamim15-5264@diu.edu.bd>
2023-02-08 22:56:05 +0600

    Create TestRec.java

---
+++ OOP/src/oop/TestRec.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package oop;
+
+/**
+ *
+ * @author Win-10
+ */
+public class TestRec {
+    public static void main(String[] args) {
+        Recursion r = new Recursion ();
+       int result= r.fact(5);
+       System.out.println(result);
+       result = r.fact(4);
+       System.out.println(result);
+    }
+    
+}
