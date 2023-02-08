ea11839f34ecbb0024d28f5f47a63a23d6b92019
MD. ABDUL HAMIM <hamim15-5264@diu.edu.bd>
2023-02-08 22:56:37 +0600

    Create C.java

---
+++ OOP/src/interfase/C.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package interfase;
+
+/**
+ *
+ * @author Win-10
+ */
+public class C implements A,B{
+    public void play (){
+        System.out.println("Hello i'm from C");
+    }
+}
