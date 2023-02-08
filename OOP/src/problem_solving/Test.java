8cf2c3dc15fc1d671270f051c7d29063bf40553a
MD. ABDUL HAMIM <hamim15-5264@diu.edu.bd>
2023-02-08 22:54:09 +0600

    Create Test.java

---
+++ OOP/src/problem_solving/Test.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package problem_solving;
+
+/**
+ *
+ * @author Win-10
+ */
+public class Test {
+    public static void main(String[] args) {
+        Shape s1;
+        s1 = new Rectangle (10,20);
+        s1.area();
+        s1 = new Triangle (10,20);
+        s1.area();
+        s1 = new Cricle (10);
+        s1.area();
+    }
+}
