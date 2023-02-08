fc8d470e517cea7f15f5b4f47c6f4a8c256b289e
MD. ABDUL HAMIM <hamim15-5264@diu.edu.bd>
2023-02-08 22:56:54 +0600

    Create Teacher.java

---
+++ OOP/src/inheritance/Teacher.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package inheritance;
+
+/**
+ *
+ * @author Win-10
+ */
+public class Teacher extends Person {
+    String quali;
+    void printinfo2(){
+        printinfo();
+        System.out.println("Qualification: "+quali);
+    }
+    
+    
+}
