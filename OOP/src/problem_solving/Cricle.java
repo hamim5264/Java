a61b9b7ff84c9d17a23a2b4899f484426a12a398
MD. ABDUL HAMIM <hamim15-5264@diu.edu.bd>
2023-02-08 22:54:23 +0600

    Create Cricle.java

---
+++ OOP/src/problem_solving/Cricle.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package problem_solving;
+
+/**
+ *
+ * @author Win-10
+ */
+public class Cricle extends Shape{
+    Cricle (double r){
+        super (r,r);
+    }
+    void area (){
+        double result = Math.PI * d1 *d2;
+        System.out.println("Area: "+result);
+    }
+}
