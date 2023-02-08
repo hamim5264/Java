28c346def39cc77112bff28d5adfbb888b91082b
MD. ABDUL HAMIM <hamim15-5264@diu.edu.bd>
2023-02-08 22:57:29 +0600

    Create Test.java

---
+++ OOP/src/Abstract/Test.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Abstract;
+
+/**
+ *
+ * @author Win-10
+ */
+public class Test {
+    public static void main(String[] args) {
+        MobileUser mu; //reference variable
+        mu = new A ();
+        mu.sendmassage();
+        mu = new B ();
+        mu.sendmassage();
+    }
+    
+}
