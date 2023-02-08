5549ab0c860e5e2ee1d7f03bd668f5083975331b
MD. ABDUL HAMIM <hamim15-5264@diu.edu.bd>
2023-02-08 22:55:10 +0600

    Create Testsuper.java

---
+++ OOP/src/Overriding/Testsuper.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Overriding;
+
+/**
+ *
+ * @author Win-10
+ */
+public class Testsuper {
+    public static void main(String[] args) {
+        Superkey2 s = new Superkey2 ();
+        s.print();
+    }
+    
+}
