b81b561afcd25aac866da92b08b1e5b051b3ec1e
MD. ABDUL HAMIM <hamim15-5264@diu.edu.bd>
2023-02-08 22:54:51 +0600

    Create Shape.java

---
+++ OOP/src/Polymorphism/Shape.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Polymorphism;
+
+/**
+ *
+ * @author Win-10
+ */
+public class Shape {
+    double area(){
+        System.out.print("Area of shape:");
+        return 0;
+    }
+    
+}
