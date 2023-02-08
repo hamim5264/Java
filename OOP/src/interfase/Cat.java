20cf1abd89e559dd53e68e806f126294fb10a5fc
MD. ABDUL HAMIM <hamim15-5264@diu.edu.bd>
2023-02-08 22:56:34 +0600

    Create Cat.java

---
+++ OOP/src/interfase/Cat.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package interfase;
+
+/**
+ *
+ * @author Win-10
+ */
+public class Cat implements Animal {
+    @Override
+    public void eat (){
+        System.out.println("Cat can eat");
+    }
+}
