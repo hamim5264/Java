50a39dcbc7264903e85f5fbec2af665907eae322
MD. ABDUL HAMIM <hamim15-5264@diu.edu.bd>
2023-02-08 22:57:33 +0600

    Create B.java

---
+++ OOP/src/Abstract/B.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Abstract;
+
+/**
+ *
+ * @author Win-10
+ */
+public class B extends MobileUser {
+    @Override
+    void sendmassage (){
+        System.out.println("Hi, I'm Leon");
+    }
+    
+}
