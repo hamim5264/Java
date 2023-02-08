73277a28615ca34e3c8582e106a40f092168b3d2
MD. ABDUL HAMIM <hamim15-5264@diu.edu.bd>
2023-02-08 22:55:52 +0600

    Create B.java

---
+++ OOP/src/Overriding/B.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Overriding;
+
+/**
+ *
+ * @author Win-10
+ */
+public class B extends A {
+    int x= 5;
+    void print(){
+        System.out.println(super.x);
+    }
+    
+}
