399ef1e2e97ef9c7290c9d7d21593e2b20f73f18
MD. ABDUL HAMIM <hamim15-5264@diu.edu.bd>
2023-02-08 22:54:25 +0600

    Create Box_Vol.java

---
+++ OOP/src/problem_solving/Box_Vol.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package problem_solving;
+
+/**
+ *
+ * @author Win-10
+ */
+public class Box_Vol {
+    public static void main(String[] args) {
+        Box b1 = new Box (10,10,10);
+        Box b2 = new Box (20,30,10);
+        b1.displayVol();
+        b2.displayVol();
+    }
+    
+}
