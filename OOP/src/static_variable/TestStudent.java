778cc512afc9e757e13780e1957cb23cdbf3b124
MD. ABDUL HAMIM <hamim15-5264@diu.edu.bd>
2023-02-08 22:53:52 +0600

    Create TestStudent.java

---
+++ OOP/src/static_variable/TestStudent.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package static_variable;
+
+/**
+ *
+ * @author Win-10
+ */
+public class TestStudent {
+    public static void main(String[] args) {
+        Student s1 = new Student () ;
+        s1.printstudent();
+        Student s2 = new Student ();
+        s2.printstudent();
+        Student s3 = new Student ();
+        s3.printstudent();
+    }
+    
+}
