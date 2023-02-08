93cda117ff470706d7c3a2d4caeb57dc93063c64
MD. ABDUL HAMIM <hamim15-5264@diu.edu.bd>
2023-02-08 22:57:18 +0600

    Create Testencap.java

---
+++ OOP/src/encap/Testencap.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package encap;
+
+/**
+ *
+ * @author Win-10
+ */
+public class Testencap {
+    public static void main(String[] args) {
+        Person p1 = new Person ();
+        p1.setName("Hamim");
+        p1.setAge(21);
+        System.out.println(p1.getName());
+        System.out.println(p1.getAge());
+    }
+    
+}
