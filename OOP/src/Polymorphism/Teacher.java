8eba427afff4309d95628c5a499ae1ef2812d027
MD. ABDUL HAMIM <hamim15-5264@diu.edu.bd>
2023-02-08 22:54:46 +0600

    Create Teacher.java

---
+++ OOP/src/Polymorphism/Teacher.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Polymorphism;
+
+/**
+ *
+ * @author Win-10
+ */
+public class Teacher extends Person{
+    @Override
+    void display (){
+        System.out.println("I am a teacher");
+    }
+    
+}
