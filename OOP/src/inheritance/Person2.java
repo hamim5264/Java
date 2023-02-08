6c177fb89b0ef602be38c59967165d6d23c584aa
MD. ABDUL HAMIM <hamim15-5264@diu.edu.bd>
2023-02-08 22:56:58 +0600

    Create Person2.java

---
+++ OOP/src/inheritance/Person2.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package inheritance;
+
+/**
+ *
+ * @author Win-10
+ */
+public class Person2 {
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+    
+    
+}
