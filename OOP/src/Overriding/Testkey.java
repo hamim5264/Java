1fb85a6586c7bc052e7fb6d15d21839c0e13857d
MD. ABDUL HAMIM <hamim15-5264@diu.edu.bd>
2023-02-08 22:55:13 +0600

    Create Testkey.java

---
+++ OOP/src/Overriding/Testkey.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Overriding;
+
+/**
+ *
+ * @author Win-10
+ */
+public class Testkey {
+    public static void main(String[] args) {
+        B b1 = new B ();
+        b1.print();
+    }
+    
+}
