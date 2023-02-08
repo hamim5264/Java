812a64b9a42c8f9b1f4f687c5227ab0cc3d3638c
MD. ABDUL HAMIM <hamim15-5264@diu.edu.bd>
2023-02-08 22:54:02 +0600

    Create StaticMethod.java

---
+++ OOP/src/Static/StaticMethod.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Static;
+
+/**
+ *
+ * @author Win-10
+ */
+public class StaticMethod {
+    void display1(){
+        System.out.println("Non static method");
+    }
+    static void display2(){
+        System.out.println("Static method");
+    }
+    
+}
