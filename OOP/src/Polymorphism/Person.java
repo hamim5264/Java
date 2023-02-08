0e2cb5987488518a2526c7aa8feb221c7fbc1a80
MD. ABDUL HAMIM <hamim15-5264@diu.edu.bd>
2023-02-08 22:54:57 +0600

    Create Person.java

---
+++ OOP/src/Polymorphism/Person.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Polymorphism;
+
+/**
+ *
+ * @author Win-10
+ */
+public class Person {
+    void display (){
+        System.out.println("I am a person");
+    }
+    
+}
