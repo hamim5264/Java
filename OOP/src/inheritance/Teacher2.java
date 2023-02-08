6741baafd4257966a1b94a2b05fa1be0d02e1665
MD. ABDUL HAMIM <hamim15-5264@diu.edu.bd>
2023-02-08 22:56:52 +0600

    Create Teacher2.java

---
+++ OOP/src/inheritance/Teacher2.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package inheritance;
+
+/**
+ *
+ * @author Win-10
+ */
+public class Teacher2 extends Person2{
+    private String quali;
+
+    public String getQuali() {
+        return quali;
+    }
+
+    public void setQuali(String quali) {
+        this.quali = quali;
+    }
+   
+    
+}
