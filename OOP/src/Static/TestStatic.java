54b039a8ed65534852a01cbfb29b26494782143a
MD. ABDUL HAMIM <hamim15-5264@diu.edu.bd>
2023-02-08 22:53:59 +0600

    Create TestStatic.java

---
+++ OOP/src/Static/TestStatic.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Static;
+
+/**
+ *
+ * @author Win-10
+ */
+public class TestStatic {
+    public static void main(String[] args) {
+     StaticMethod o1 = new StaticMethod ();
+    o1.display1();
+    
+    StaticMethod.display2();
+    }
+    
+    
+}
