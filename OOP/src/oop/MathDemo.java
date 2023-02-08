ae30a65deb1ee79cfee8f1fedeac69d52cc760db
MD. ABDUL HAMIM <hamim15-5264@diu.edu.bd>
2023-02-08 22:56:17 +0600

    Create MathDemo.java

---
+++ OOP/src/oop/MathDemo.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package oop;
+
+/**
+ *
+ * @author Win-10
+ */
+public class MathDemo {
+    public static void main (String[] args){
+        System.out.println(Math.abs(-9.5));
+        System.out.println(Math.sqrt(25));
+        System.out.println(Math.pow(2, 3));
+        System.out.println(Math.PI);
+        System.out.println(Math.log(1));
+        System.out.println(Math.exp(1));
+        System.out.println(Math.max(-2.5, 5));
+        System.out.println(Math.min(-2.5, 5));
+        System.out.println(Math.ceil(-5.6));
+        System.out.println(Math.floor(-5.6    ));
+    }
+    
+}
